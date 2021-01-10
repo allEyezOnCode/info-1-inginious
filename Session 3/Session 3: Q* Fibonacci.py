826c942abbed3d7b60e120c9f2f3a0d74b3532db
Alexandre Dewilde <45940897+allEyezOnCode@users.noreply.github.com>
2021-01-10 10:43:24 +0100

    Update Session 3: Q* Fibonacci.py

--- Session 3/Session 3: Q* Fibonacci.py
+++ Session 3/Session 3: Q* Fibonacci.py
@@ -1,6 +1,15 @@
 def fibonacci(n):
     if n <= 0:
         return 0
-    if n <= 2:
-        return 1
-    return fibonacci(n-1) + fibonacci(n-2)
+    first = second = 1
+    for i in range(2, n):
+        first, second = second, first + second
+    return second
+
+# Recursive approach inefficient
+# def fibonacci(n):
+#     if n <= 0:
+#         return 0
+#     if n <= 2:
+#         return 1
+#     return fibonacci(n-1) + fibonacci(n-2)
